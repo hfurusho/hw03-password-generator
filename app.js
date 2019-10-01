40f5e08c6c1c19dc0995f6836e824a89e2c720c8
Kami <51175640+hfurusho@users.noreply.github.com>
2019-09-30 22:08:26 -0700

    Added function to prompt user for length of password and check if the user input is valid.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var specialCharsRange = ["33-47", "58-64", "91-96", "123-126"];
-var numbersRange = "48-57";
-var uppercaseRange = "65-90";
-var lowercaseRange = "97-122";
+// For reference. These are the character codes if I need to use String.fromCharCode(charCode)
+// var specialCharsRange = ["33-47", "58-64", "91-96", "123-126"];
+// var numbersRange = "48-57";
+// var uppercaseRange = "65-90";
+// var lowercaseRange = "97-122";
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const lowercaseChars = [
@@ -95,17 +96,33 @@ const specialChars = [
   "~"
 ];
 
-var numsReq = true;
-var uppercaseReq = true;
-var lowercaseReq = true;
-var specialsReq = true;
+var pwLength;
+var numsReq = false;
+var lowercaseReq = false;
+var uppercaseReq = false;
+var specialsReq = false;
+
+function userPrompts() {
+  if (pwLength == undefined) {
+    pwLength = Number(
+      prompt(
+        "How many characters should the password be? Please choose a number between 8 and 128."
+      )
+    );
+    while (Number.isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
+      pwLength = Number(
+        prompt(
+          "Please enter a valid number. How many characters should the password be? Choose a number between 8 and 128."
+        )
+      );
+    }
+  }
+}
 
 // Returns an array of the available characters based on the user's password requirements.
 function generateCharSet() {
   let charSet = [];
-  console.log(numsReq, uppercaseReq, lowercaseReq, specialsReq);
   if (numsReq) {
-    console.log(charSet);
     charSet.push(...numbers);
   }
   if (lowercaseReq) {
@@ -117,12 +134,12 @@ function generateCharSet() {
   if (specialsReq) {
     charSet.push(...specialChars);
   }
-  console.log(charSet);
   return charSet;
 }
 
 var charSet = generateCharSet();
 
+// Returns a string of random characters of a given length.
 function generateRandomPassword(pwLength) {
   let generatedPassword = "";
   let randomNumber;
